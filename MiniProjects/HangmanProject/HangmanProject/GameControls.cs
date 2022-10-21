59e1aa56fc2353beda0ff5a1ebb35924fca29c39
Briankip1 <devbriankip@gmail.com>
2022-10-21 15:29:43 -0500

    training

---
+++ MiniProjects/HangmanProject/HangmanProject/GameControls.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanProject
+    
+{
+    
+    internal class GameControls
+    {
+        public string[] household;
+        char[] character;
+
+        public string randomWords()
+        {
+            Random words = new Random();
+            words.Next(words.Length);
+        }
+
+        public string Splitwords()
+        {
+            for (int i = 0; i < household.Length; i++)
+            {
+                char[] character = household[i].ToCharArray();
+
+                if ()
+
+        }
+        }
+    }
+}
---
+++ MiniProjects/HangmanProject/HangmanProject/Player.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanProject
+{
+    public class Player
+    {
+        public string FirstName;
+        public string LastName;
+        public bool Win;
+        public string guessLetter;
+
+            public string Move1()
+            {
+                if (guessLetter != character)
+                {
+                    Console.WriteLine("___|-");
+                    return guessLetter;
+                }
+                else
+                {
+                    Console.WriteLine("It is the correct letter.. continue");
+                    return guessLetter;
+                }
+            }
+
+            public string Move2()
+            {
+                if (guessLetter != character)
+                {
+                    Console.WriteLine("___|--");
+                    return guessLetter;
+                }
+                else
+                {
+                    Console.WriteLine("It is the correct letter.. continue");
+                    return guessLetter;
+                }
+            }
+
+            public string Move3()
+            {
+                if (guessLetter != character)
+                {
+                    Console.WriteLine("___|---");
+                    return guessLetter;
+                }
+                else
+                {
+                    Console.WriteLine("It is the correct letter.. continue");
+                    return guessLetter;
+                }
+            }
+
+            public string Move4()
+            {
+                if (guessLetter != character)
+                {
+                    Console.WriteLine("___|----");
+                    return guessLetter;
+                }
+                else
+                {
+                    Console.WriteLine("It is the correct letter.. continue");
+                    return guessLetter;
+                }
+            }
+
+            public string Move5()
+            {
+                if (guessLetter != character)
+                {
+                    Console.WriteLine("___|-----");
+                    return guessLetter;
+                }
+                else
+                {
+                    Console.WriteLine("It is the correct letter.. continue");
+                    return guessLetter;
+                }
+            }
+
+            public string Move6()
+            {
+                if (guessLetter != character)
+                {
+                    Console.WriteLine("___|------");
+                    return guessLetter;
+                }
+                else
+                {
+                    Console.WriteLine("It is the correct letter.. continue");
+                    return guessLetter;
+                }
+            }
+
+            public void Move7()
+            {
+                if (guessLetter != character)
+                {
+                    Console.WriteLine("___|------0");
+                    Console.WriteLine("Game Over \nPress Any Key to Try Again\n");
+                    Console.ReadKey();
+                }
+            }
+
+            public Player(string first, string last, bool w)
+            {
+                FirstName = first;
+                LastName = last;
+                Win = w;
+
+            }
+     
+    
+
+}
+
+}
+
