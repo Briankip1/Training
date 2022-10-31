5dcb21d0475e70845da27a6efa037d7f8142a740
Briankip1 <devbriankip@gmail.com>
2022-10-31 16:10:04 -0500

    adding hangman to main repo

---
+++ MiniProjects/HagmanTrial/GameClass/Hangman.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClass
+{
+    public class Hangman
+    {
+
+
+
+        public int Lives;
+        public string ChosenWord = " ";
+        public List<string> Letters = new List<string>();
+
+        public List<string> RandomWords;
+        //    {
+          //  "apples",
+            //"kiwi",
+            //"Avocado",
+            //"Bananas" };
+    
+
+        public Hangman(int lives, List<string> rwords)
+        {
+            this.Lives = lives;
+            this.RandomWords = rwords;
+            this.ChosenWord = Randomize();
+        }
+
+        //public GameOne(List<string> RandomWords)
+        //{
+        //    string word1;
+        //    string word2;
+        //    string word3;
+        //    string word4;
+        //}
+        public string Randomize()
+        {
+            Random random = new Random();
+
+            return  RandomWords[random.Next(0, RandomWords.Count)];
+            
+        }
+
+        public void CheckingLetters()
+        {
+            while (Lives != 0)
+            {
+                int charactersLeft = 0;
+
+                foreach (string word in RandomWords)
+                {
+                    string letter = word.ToString();
+                    if (Letters.Contains(letter))
+                    {
+                        Console.Write(letter);
+                    }
+
+                    else
+                    {
+                        Console.Write("_");
+
+                        charactersLeft++;
+                    }
+
+                }
+                if (charactersLeft == 0)
+                {
+                    break;
+                }
+            }
+        }
+
+        public void HandleGuess()
+        {
+
+            var key = Console.ReadKey().Key.ToString().ToLower();
+
+
+            if (Letters.Contains(key))
+            {
+
+                Console.WriteLine("You already entered this letter!");
+            }
+
+
+            Letters.Add(key);
+
+
+            if (!ChosenWord.Contains(key))
+            {
+                Lives--;
+
+            }
+            
+            if (Lives > 0)
+            {
+
+                Console.WriteLine($"The letter {key} is not in the word. You have {Lives} {(Lives == 1 ? "try" : "tries")} left.");
+            }
+            
+
+            if (Lives > 0)
+            {
+
+                Console.WriteLine($"You won with {Lives} {(Lives == 1 ? "life" : "lives")} left!");
+            }
+            else
+            {
+                Console.WriteLine($"You lost! The word was {ChosenWord}.");
+            }
+        }
+    }
+}
+
+
---
+++ MiniProjects/HagmanTrial/HagmanTrial/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// a pool words to be randomized, in form of an array
+using GameClass;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+using System.Collections;
+
+//Console.WriteLine("Enter the guess characters below...");
+//Console.ReadLine();
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Input a letter here ...");
+        var RandomWords = new List<string>()
+        {
+            "apples",
+            "kiwi",
+            "Avocado"
+        };
+
+        Hangman GameOne = new Hangman(6, RandomWords);
+
+        GameOne.HandleGuess();
+   
+
+        GameOne.CheckingLetters();
+        
+
+
+
+
+
+
+    
+       
+            
+      
+
+              
+    }
+}
