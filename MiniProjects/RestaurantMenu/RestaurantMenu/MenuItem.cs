4150b186895ca8451e9021bc6c54bbf323bd7a8b
Briankip1 <devbriankip@gmail.com>
2022-11-30 15:08:53 -0600

    Add the restaurant menu project, to the mini projects, check it

---
+++ MiniProjects/RestaurantMenu/RestaurantMenu/MenuItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    public  class MenuItem
+    {
+        public string name;
+        public int calories;
+        public float price;
+
+        public MenuItem(string name, int calories, float price)
+        {
+            this.name = name;
+            this.calories = calories;
+            this.price = price;
+            Print();
+        } 
+        
+        public void Print()
+        {
+            Console.WriteLine($"{name}\n, {calories}\n, {price}");
+        }
+    }
+}
