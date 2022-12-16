796a5f2c8dcb4aee407794f2e435c05b51dc97ff
Briankip1 <devbriankip@gmail.com>
2022-12-16 05:29:33 -0600

    Add thr BirdWatcher and Interest is Interesting Projects

---
+++ MiniProjects/Exercism Assignments/BirdWatcher/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+static class SavingsAccount
+{
+    public static float InterestRate(decimal balance)
+    {
+        while (balance != 0)
+        {
+            if (balance < 0)
+            {
+                return 3.213f;
+            }
+            if (balance < 1000)
+            {
+                return 0.5f;
+            }
+            if ((balance >= 1000) && (balance < 5000))
+            {
+                return 1.621f;
+            }
+            if (balance >= 5000)
+            {
+                return 2.475f;
+            }
+        }
+        return 0.5f;
+        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
+    }
+
+    public static decimal Interest(decimal balance)
+    {
+        decimal currentInterest = (balance * (decimal)InterestRate(balance)) / 100;
+        return currentInterest;
+
+        throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
+    }
+
+    public static decimal AnnualBalanceUpdate(decimal balance)
+    {
+        return balance + Interest(balance);
+        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
+    }
+
+    public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+    {
+        int years = 0;
+        while (targetBalance > balance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            years++;
+        }
+        return years;
+        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+    }
+}
+
--- MiniProjects/RestaurantMenu/RestaurantMenu/MenuTest.cs
+++ MiniProjects/RestaurantMenu/RestaurantMenu/MenuTest.cs
@@ -12,20 +12,23 @@ namespace RestaurantMenu
         {
             Menu menu = new Menu("BurgerKing");
             menu.Description = "The Best in town\n";
-            MenuItem item3 = new MenuItem("Chicken Burrito", 465, 8.99f);
+            MenuItem item3 = new MenuItem("Creamy Garlic Chicken Breast", 465, 8.99f);
             menu.Add(item3);
             MenuItem item4 = new MenuItem("Large Burger", 516, 7.98f);
             menu.Add(item4);
             //menu.Total();
             menu.Print();
-
+            item4.calories = 500;
+            menu.Print();
             Menu menu1 = new Menu("Original Pancake");
             menu1.Description = "Leading since 1985\n";
             MenuItem item5 = new MenuItem("American Style Pancake\t", 303, 3.59f);
             menu1.Add(item5);
             menu1.Print();
 
-
+            List<MenuItem> order = new List<MenuItem>();
+            order.Add(menu.Items[0]);
+            Console.WriteLine(menu.Total(order));
             Console.ReadLine();
         }
     }
