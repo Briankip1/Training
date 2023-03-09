db2d9a59cf725e40ab32c6a03ec2f62e89f05bf9
Briankip1 <devbriankip@gmail.com>
2023-03-09 16:41:40 -0600

    Udpated accountability app

--- MiniProjects/AccountabilityApp/Goal.cs
+++ MiniProjects/AccountabilityApp/Goal.cs
@@ -2,7 +2,37 @@
 
 public class Goal
 {
-	public Goal()
+	public List<string> Categories;
+	public DateTime Duration;
+	public List<String> Partners;
+
+	// Matches the participant entries with the list of available categories
+	public void CategoryMatching()
 	{
+
+	}
+
+	// Determines whether the goal a success or failure based on the update from the participant
+	public void GoalResult()
+	{
+
+	}
+
+	//Award points based on success on the update, success or failure of the goal
+	public void GoalPoints()
+	{
+
+	}
+
+	//Each update elevates takes the goal a step closer to accomplishment
+	public void LevelUpdateOnGoalAccomplishment()
+	{
+
+	}
+
+	//Generate reports based on the dates and goal progress
+	public void GeneratePeriodicProgressReports()
+	{
+
 	}
 }
--- MiniProjects/AccountabilityApp/Participant.cs
+++ MiniProjects/AccountabilityApp/Participant.cs
@@ -1,8 +1,33 @@
 ﻿using System;
 
-public class Class1
+public class Participant
 {
-	public Class1()
+	public Guid Id; 
+	public string Name;
+	public string Email;
+	public int Age;
+
+	// pariticipant enters the goals they want to accomplish
+	public void GoalEntry() 
 	{
+
 	}
+	// Enables the participant to buy stakes that can be lost or maintained based on their goals update, success or failure
+	public void BuyGoalStakes()
+	{
+
+	}
+	// The participant updates on whether they progress on the goal or not
+	public void GoalUpdateProgress()
+	{
+
+	}
+
+
+	// Participant determines whether they want a partner or they can self referee
+	public void PickPartnerorSelfReferee()
+	{
+
+	}
+
 }
